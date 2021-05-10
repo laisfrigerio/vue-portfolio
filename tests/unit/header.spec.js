9ee0deb5e054addd573cd70a3d8f845849361973
Lais Frigério <laaisfrigerio@gmail.com>
2021-05-09 21:34:00 -0300

    Add test to check what link from header is active

--- tests/unit/header.spec.js
+++ tests/unit/header.spec.js
@@ -6,4 +6,9 @@ describe('TheHeader.vue', () => {
     const wrapper = shallowMount(TheHeader)
     expect(wrapper.findAll('header ul li')).toHaveLength(3)
   })
+
+  it('check "Início" link is active', () => {
+    const wrapper = shallowMount(TheHeader)
+    expect(wrapper.find('header ul li:first-child').classes()).toContain('active')
+  })
 })
